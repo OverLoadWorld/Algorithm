8719cbbaf4cf3ead9fa09bb6cf926eac93572476
xdc <ZY_DPG@outlook.com>
2018-09-26 13:30:28 +0800

    :tada: add max_presuf kmp function achive

--- KMP/main.cpp
+++ KMP/main.cpp
@@ -1,6 +1,71 @@
 #include <iostream>
+#include <iomanip>
+#include <string>
 using namespace std;
+/* function:solve max prefix surfirx
+ * input: 
+ * output:
+ *
+ */
+int max_presuf(string str,int array[]){ 
+    int ret = -1;
+    int length = str.length();
+    int pre_end = 0;
+    if(length == 0){
+        cout << "pattern string is empty" << endl;
+        return ret;
+    }
+    for(int index = 1;index < length;index++){
+        while((pre_end >0 )&&(str[pre_end]!=str[index])){
+            pre_end = array[pre_end - 1];
+        }
+        if(str[pre_end] == str[index]){
+            pre_end++;
+        }  
+            array[index] = pre_end;
+    }    
+    return ret;
+};
+int kmp(string ts,string ps,int array[]){
+    int t_length = ts.size();
+    int p_length = ps.size();
+    int pre_end = 0;
+    if(t_length == 0 || p_length == 0){
+        return -1;
+    }
+    //solve max prefix suffix array
+    max_presuf(ps,array);
+    for(int index = 0;index < t_length;index++){
+       while(pre_end > 0 && ps[pre_end] != ts[index]){
+            pre_end = array[pre_end - 1];
+       }
+       if(ps[pre_end] == ts[index]){
+            pre_end++;
+       }
+       if(pre_end == p_length){
+           return index - p_length + 1;
+       }
+   } 
+   return -1;
+}
 
 int main(){
+    string ts;  //target string
+    string ps;  //pattern string
+    cout << "pleaase input pattern string:"<<endl;
+    cin >> ps;
+    int length = ps.size();
+    int *F = new int[length];
+    for(int index = 0;index < length;index ++){
+        F[index] = -1;
+    }
+    F[0] = 0;
+    cout << "please input target string:" << endl;
+    cin >> ts;
+    int position = kmp(ts,ps,F);
+    for(int index = 0;index < length;index++){
+        cout <<ps.substr(0,index + 1) <<": "<<right<<setw(length - index)<<F[index]<<endl;
+    }
+    cout <<"position:" << position <<endl;
     return 0;
 }
--- KMP/readme.txt
+++ KMP/readme.txt
@@ -1,7 +1,9 @@
 KMP算法:
-KMP算法求解什么类型问题
-字符串匹配。给你两个字符串，寻找其中一个字符串是否包含另一个字符串，如果包含，返回包含的起始位置。
-    char *str = "bacbababadababacambabacaddababacasdsd";
-    char *ptr = "ababaca";
-    str有两处包含ptr 
-    分别在str的下标10，26处包含ptr。
+    1.KMP算法求解什么类型问题
+        字符串匹配。给你两个字符串，寻找其中一个字符串是否包含另一个字符串，如果包含，返回包含的起始位置。
+        char *str = "bacbababadababacambabacaddababacasdsd";
+        char *ptr = "ababaca";
+        str有两处包含ptr 
+        分别在str的下标10，26处包含ptr。
+    2.在匹配过程中一步重要的工作就是next数组的求解，next数组的求解代码就短短的几行，但是理解起来还得花费一点功夫，
+      后续我会再补充next数组的详细解析，本例中的代码用F代表next数组，便于理解。          
