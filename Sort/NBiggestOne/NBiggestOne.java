36498c391a1ee824aec3575735633e8c49feea94
howard <zhaozhenhua1@xiaomi.com>
2019-02-15 14:47:28 +0800

    find n biggest num

--- Sort/NBiggestOne/NBiggestOne.java
+++ Sort/NBiggestOne/NBiggestOne.java
@@ -8,32 +8,40 @@ import QuickSort.QuickSort;
  * @Date: 19-2-14 下午7:31
  */
 public class NBiggestOne {
-    /*public static void main(String[] args) {
+    public static void main(String[] args) {
         int[] a = {6,3,8,1,9,7,2};
-        System.out.println(findNBiggest(a, 0, a.length - 1, 3));
+        System.out.println(findNBiggest(a, 7));
     }
 
-    private static int findNBiggest(int[] a, int start, int end, int N) {
+    private static int findNBiggest(int[] a, int n) {
+        return find(a, 0, a.length - 1, n);
+    }
+
+    private static int find(int[] a, int start, int end, int n) {
+        int pIndex = partition(a, start, end);
+        if (pIndex + 1 == n) {
+            return a[pIndex];
+        } else if (pIndex + 1 > n) {
+            return find(a, start, pIndex - 1, n);
+        } else {
+            return find(a, pIndex + 1, end, n);
+        }
+    }
+
+    private static int partition(int[] a, int start, int end) {
         int p = a[end];
-        int i = start;
-        for (int j = start; j < end; j++) {
+        int i = start, j = start;
+        for (; j <= end; j++) {
             if (a[j] < p) {
-                int t = a[i];
-                a[i] = a[j];
-                a[j] = t;
+                int t = a[j];
+                a[j] = a[i];
+                a[i] = t;
                 i++;
             }
         }
         int t = a[i];
-        a[i] = a[end];
+        a[i] = p;
         a[end] = t;
-
-        if (p + 1 == N) {
-            return a[p];
-        } else if (p + 1 > N) {
-            return findNBiggest(a, start, p - 1, N);
-        } else {
-            return findNBiggest(a, p + 1, end, N);
-        }
-    }*/
+        return i;
+    }
 }
