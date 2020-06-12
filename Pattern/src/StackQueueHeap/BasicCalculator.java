5518d2da57602052b60011063b3a22a28204fea6
zhaozhenhua <zhaozhenhua1@xiaomi.com>
2020-06-12 12:02:28 +0800

    t

--- Pattern/src/StackQueueHeap/BasicCalculator.java
+++ Pattern/src/StackQueueHeap/BasicCalculator.java
@@ -1,5 +1,38 @@
 package StackQueueHeap;
 
+import com.sun.org.apache.regexp.internal.RE;
+
+import java.util.Stack;
+
 //224
 public class BasicCalculator {
+
+    public int calculate(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (int i = s.length() - 1; i >= 0; i--) {
+            if (s.charAt(i) != ' ') {
+                stack.push(s.charAt(i));
+            }
+        }
+
+        return calculateStack(stack);
+    }
+
+    public int calculateStack(Stack<Character> stack) {
+        int result = 0;
+
+        while (stack.peek() != null) {
+            if (stack.peek() >= '0' && stack.peek() <= '9') {
+
+            }
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new BasicCalculator().calculate("1 + 1"));
+        System.out.println(new BasicCalculator().calculate(" 2-1 + 2 "));
+        System.out.println(new BasicCalculator().calculate("(1+(4+5+2)-3)+(6+8)"));
+    }
 }
